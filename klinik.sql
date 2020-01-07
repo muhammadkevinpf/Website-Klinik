e6fcedf450c0fc66273425f53790e5e9311f59b2
muhammadkevinpf <muhammadkevinpf@gmail.com>
2020-01-07 23:30:43 +0700

    init commit

--- klinik.sql
+++ klinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2019 at 07:56 PM
+-- Generation Time: May 21, 2019 at 04:29 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -61,20 +61,21 @@ INSERT INTO `detailjadwal` (`id`, `idDokter`, `idJadwal`, `hari`) VALUES
 
 CREATE TABLE `dokter` (
   `id` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL
+  `nama` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `dokter`
 --
 
-INSERT INTO `dokter` (`id`, `nama`) VALUES
-(1, 'Jafar'),
-(2, 'Elsa'),
-(3, 'Ghaisani'),
-(4, 'Indah'),
-(5, 'Chrisna'),
-(6, 'Monica');
+INSERT INTO `dokter` (`id`, `nama`, `harga`) VALUES
+(1, 'Jafar', 65000),
+(2, 'Elsa', 50000),
+(3, 'Ghaisani', 65000),
+(4, 'Indah', 50000),
+(5, 'Chrisna', 65000),
+(6, 'Monica', 50000);
 
 -- --------------------------------------------------------
 
@@ -114,7 +115,7 @@ CREATE TABLE `kontak` (
 --
 
 INSERT INTO `kontak` (`id`, `nama`, `email`, `pesan`, `created_at`) VALUES
-(14, 'Kepin', 'Muhammadkevinpf@gmail.com', 'edan lur', '2019-03-24');
+(14, 'Kepin', 'Muhammadkevinpf@gmail.com', 'edan lursdadsdaddsadasdadas', '2019-03-24');
 
 -- --------------------------------------------------------
 
@@ -126,17 +127,20 @@ CREATE TABLE `login` (
   `id` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `nik` varchar(20) NOT NULL
+  `nik` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `username`, `password`, `nik`) VALUES
-(1, 'admin', 'admin123', '123456789'),
-(2, 'atengs', '123123123', '1231231231'),
-(3, 'sad', '123123123', '3242442523532');
+INSERT INTO `login` (`id`, `username`, `password`, `nik`, `status`) VALUES
+(1, 'admin', 'admin123', '123456789', 'admin'),
+(2, 'atengs', '123123123', '1231231231', 'member'),
+(3, 'sad', '123123123', '3242442523532', 'member'),
+(4, 'jafar', 'jafar123', '0000025478', 'dokter'),
+(5, 'shalma', '12345678', '1213425453252', 'member');
 
 -- --------------------------------------------------------
 
@@ -148,6 +152,8 @@ CREATE TABLE `pendaftaran` (
   `id` int(11) NOT NULL,
   `nik` varchar(20) NOT NULL,
   `idJadwal` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL,
   `created_at` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -155,9 +161,61 @@ CREATE TABLE `pendaftaran` (
 -- Dumping data for table `pendaftaran`
 --
 
-INSERT INTO `pendaftaran` (`id`, `nik`, `idJadwal`, `created_at`) VALUES
-(2, '3242442523532', 2, '2019-03-30'),
-(5, '1231231231', 2, '2019-04-17');
+INSERT INTO `pendaftaran` (`id`, `nik`, `idJadwal`, `harga`, `status`, `created_at`) VALUES
+(4, '1231231231', 1, 190000, 'Sudah Bayar', '2019-05-19'),
+(5, '3242442523532', 2, 2900000, 'Sudah Bayar', '2019-05-19'),
+(6, '1213425453252', 1, 160000, 'Sudah Bayar', '2019-05-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_pendaftaran`
+--
+
+CREATE TABLE `temp_pendaftaran` (
+  `id` int(11) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `idJadwal` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `created_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `temp_pendaftaran`
+--
+
+INSERT INTO `temp_pendaftaran` (`id`, `nik`, `idJadwal`, `harga`, `status`, `created_at`) VALUES
+(4, '1231231231', 1, 65000, 'Sudah Diperiksa', '2019-05-19'),
+(5, '3242442523532', 2, 50000, 'Sudah Diperiksa', '2019-05-19'),
+(6, '1213425453252', 1, 65000, 'Sudah Diperiksa', '2019-05-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `treatment`
+--
+
+CREATE TABLE `treatment` (
+  `id` int(11) NOT NULL,
+  `nama_treatment` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `treatment`
+--
+
+INSERT INTO `treatment` (`id`, `nama_treatment`, `harga`) VALUES
+(1, 'Pencetakan Gigi', 120000),
+(2, 'Rontgen', 150000),
+(3, 'Bleaching Interna', 175000),
+(4, 'Tambal Gigi', 95000),
+(5, 'Veneer Porcelain', 2850000),
+(6, 'Perawatan Gigi', 150000),
+(7, 'Lepas Bracket per Rahang', 180000),
+(8, 'Replace Bracket per Gigi', 80000),
+(9, 'Scaling', 90000);
 
 -- --------------------------------------------------------
 
@@ -177,6 +235,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`nik`, `nama`, `alamat`, `tanggalLahir`) VALUES
+('0000025478', 'jafar', 'Jalan', '2019-05-19'),
+('1213425453252', 'shal', 'jl asoy', '2019-05-01'),
 ('1231231231', 'Ateng atengs', 'Jalan garu', '1998-08-31'),
 ('123456789', 'admin', 'Jl. Sini', '1998-08-21'),
 ('3242442523532', 'as', 'fhfgh', '2019-03-13');
@@ -227,6 +287,20 @@ ALTER TABLE `pendaftaran`
   ADD KEY `idJadwal` (`idJadwal`);
 
 --
+-- Indexes for table `temp_pendaftaran`
+--
+ALTER TABLE `temp_pendaftaran`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nik` (`nik`),
+  ADD KEY `idJadwal` (`idJadwal`);
+
+--
+-- Indexes for table `treatment`
+--
+ALTER TABLE `treatment`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -264,13 +338,25 @@ ALTER TABLE `kontak`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `temp_pendaftaran`
+--
+ALTER TABLE `temp_pendaftaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `treatment`
+--
+ALTER TABLE `treatment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
@@ -295,6 +381,13 @@ ALTER TABLE `login`
 ALTER TABLE `pendaftaran`
   ADD CONSTRAINT `pendaftaran_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `users` (`nik`),
   ADD CONSTRAINT `pendaftaran_ibfk_2` FOREIGN KEY (`idJadwal`) REFERENCES `detailjadwal` (`id`);
+
+--
+-- Constraints for table `temp_pendaftaran`
+--
+ALTER TABLE `temp_pendaftaran`
+  ADD CONSTRAINT `temp_pendaftaran_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `users` (`nik`),
+  ADD CONSTRAINT `temp_pendaftaran_ibfk_2` FOREIGN KEY (`idJadwal`) REFERENCES `jadwal` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
